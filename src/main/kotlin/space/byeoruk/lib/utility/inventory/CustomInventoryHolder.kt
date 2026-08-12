32cd83dd4ee6cd2ef112dd08fc757408af67e01f
qufnr <qufnr@icloud.com>
2026-08-12 16:04:23 +0900

    refactor: `CustomInventoryHolder` 이벤트 분리 메소드 제거
    - 이제 `CustomInventoryHolder#onInventory*` 메소드로 처리 가능

--- src/main/kotlin/space/byeoruk/lib/utility/inventory/CustomInventoryHolder.kt
+++ src/main/kotlin/space/byeoruk/lib/utility/inventory/CustomInventoryHolder.kt
@@ -4,24 +4,14 @@ import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.event.inventory.InventoryOpenEvent
 import org.bukkit.inventory.InventoryHolder
-import org.bukkit.inventory.ItemStack
 
 interface CustomInventoryHolder : InventoryHolder {
-    fun onOpen() {}
-    fun onClose() {}
-    fun onClick(item: ItemStack, slot: Int): Boolean = true
-
     fun onInventoryOpen(event: InventoryOpenEvent) {
-        onOpen()
     }
 
     fun onInventoryClose(event: InventoryCloseEvent) {
-        onClose()
     }
 
     fun onInventoryClick(event: InventoryClickEvent) {
-        val item = event.currentItem ?: return
-        val slot = event.rawSlot
-        event.isCancelled = onClick(item, slot)
     }
 }
