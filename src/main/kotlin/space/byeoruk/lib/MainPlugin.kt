83fc8e524906d88f3363380b202fdc2ed903e77d
qufnr <qufnr@icloud.com>
2026-09-02 15:41:50 +0900

    refactor: MainPlugin internal로 변경

--- src/main/kotlin/space/byeoruk/lib/MainPlugin.kt
+++ src/main/kotlin/space/byeoruk/lib/MainPlugin.kt
@@ -3,7 +3,7 @@ package space.byeoruk.lib
 import org.bukkit.plugin.java.JavaPlugin
 import space.byeoruk.lib.utility.inventory.CustomInventoryListener
 
-class MainPlugin : JavaPlugin() {
+internal class MainPlugin : JavaPlugin() {
 
     override fun onEnable() {
         server.pluginManager.registerEvents(CustomInventoryListener(), this)
